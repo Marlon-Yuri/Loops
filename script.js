b5f54f4f31174d29f2d443b634ccbd4eb3f56ced
Marlon-Yuri <marlonyurimi@gmail.com>
2021-05-19 19:41:19 -0300

    first commit loops

---
+++ script.js
@@ -0,0 +1,31 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(i =1 ; i<=10;i++ ){
+    console.log(i)
+
+}
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let contaDecrescente = 10
+while(contaDecrescente >=1){
+    console.log(contaDecrescente --)
+}
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+for(i=1; i <= 100; i++ ){
+    if(i % 2 !=0){
+        console.log(i)
+    }
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(i=1; i <= 100; i++ ){
+    if(i % 2 ==0){
+       console.log(i)
+   }
+
+
+}
